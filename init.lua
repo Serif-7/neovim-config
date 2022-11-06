6bc2ce6b0811c4111c5c5c01a1228cbea4560a0d
Daniel Gilleran <dangilleran7@protonmail.com>
2022-11-06 17:05:55 -0500

    added hop.nvim

--- init.lua
+++ init.lua
@@ -14,6 +14,15 @@ require('packer').startup(function(use)
   -- package manager
   use 'wbthomason/packer.nvim'
 
+  use {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end
+  }
+
   -- jump to word
   use {
     'ggandor/leap.nvim',
